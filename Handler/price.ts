2516755fbee6f7ccae210f5e8b24c027044feb56
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2020-02-27 12:38:19 +0100

    FromString returns number in price

--- Handler/price.ts
+++ Handler/price.ts
@@ -6,13 +6,13 @@ import { StateEditor } from "../StateEditor"
 import { Settings } from "../Settings"
 import { add } from "./base"
 
-class Handler implements Converter<string>, Formatter {
+class Handler implements Converter<number>, Formatter {
 	constructor(readonly currency: isoly.Currency | undefined) {}
 	toString(data: string | any): string {
 		return typeof data == "string" ? data : ""
 	}
-	fromString(value: string): string | undefined {
-		return typeof value == "string" ? value : undefined
+	fromString(value: string): number | undefined {
+		return typeof value == "string" ? Number(value) : undefined
 	}
 	format(unformated: StateEditor): Readonly<State> & Settings {
 		let separator = unformated.value.includes(".") ? unformated.value.indexOf(".")
