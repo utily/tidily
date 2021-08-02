c928a379f0e94322b0f4f8bf69f0c86d2a2510ae
Magnus Gustafsson <magnus.gustafsson@intergiro.com>
2021-08-02 16:16:19 +0200

    Move Selection get Cursor and Stalker to function

--- Action.ts
+++ Action.ts
@@ -1,5 +1,7 @@
+import { Direction } from "./Direction"
 import { Formatter } from "./Formatter"
 import { getAdjecentWordBreakIndex } from "./getAdjecentWordBreakIndex"
+import { Selection } from "./Selection"
 import { Settings } from "./Settings"
 import { State } from "./State"
 import { StateEditor } from "./StateEditor"
@@ -13,15 +15,6 @@ export interface Action {
 	metaKey?: boolean
 }
 
-function showString(str: string, index: number) {
-	console.log(str)
-	let pointerString = ""
-	for (let i = 0; i <= index; i++) {
-		pointerString += i != index ? " " : "^"
-	}
-	console.log(pointerString)
-}
-
 export namespace Action {
 	export function apply(
 		formatter: Formatter,
@@ -32,17 +25,15 @@ export namespace Action {
 
 		if (action) {
 			if (action.ctrlKey) {
-				console.log("ctrlKey")
 				if (action.key == "a")
-					selectAll(formatter, state)
+					select(state, 0, state.value.length, "forward")
 				else if (["ArrowLeft", "ArrowRight"].includes(action.key) && (state as any)?.type != "password")
 					result = ctrlArrow(formatter, state, action)
 				else if (["Delete", "Backspace"].includes(action.key) && (state as any)?.type != "password")
-					result = ctrlDeleteAction(formatter, state, action)
+					result = ctrlBackspaceDelete(formatter, state, action)
 			} else if (["ArrowLeft", "ArrowRight", "Home", "End"].includes(action.key))
-				result = arrowHomeEnd(formatter, state, action)
+				arrowHomeEnd(result, action)
 			else if (["Delete", "Backspace"].includes(action.key)) {
-				console.log("Delete or Backspace")
 				result.selection.start == result.selection.end &&
 					select(
 						result,
@@ -50,71 +41,39 @@ export namespace Action {
 						result.selection.end + (action.key == "Delete" ? 1 : 0)
 					)
 				erase(result)
-			} else if (action.key != "Unidentified" && formatter.allowed(action.key, result)) {
-				console.log("insert")
-				insert(result, action.key)
+			} else if (action.key != "Unidentified") {
+				erase(result)
+				formatter.allowed(action.key, result) && insert(result, action.key)
 			}
 		}
 		return formatter.format(StateEditor.copy(result))
 	}
 
 	function ctrlArrow(formatter: Formatter, state: Readonly<State>, action: Action): Readonly<State> {
-		let cursorPosition = state.selection.direction == "backward" ? state.selection.start : state.selection.end
-		let otherPosition = cursorPosition == state.selection.start ? state.selection.end : state.selection.start
+		let cursorPosition = Selection.getCursor(state.selection)
+		let stalkPosition = Selection.getStalker(state.selection)
 		cursorPosition = getAdjecentWordBreakIndex(
 			state.value,
 			cursorPosition,
 			action.key == "ArrowLeft" ? "backward" : "forward"
 		)
-		otherPosition = action.shiftKey ? otherPosition : cursorPosition
-
+		stalkPosition = action.shiftKey ? stalkPosition : cursorPosition
 		return State.copy(
 			formatter.unformat(
 				StateEditor.copy({
 					...state,
 					selection: {
-						start: Math.min(otherPosition, cursorPosition),
-						end: Math.max(otherPosition, cursorPosition),
+						start: Math.min(stalkPosition, cursorPosition),
+						end: Math.max(stalkPosition, cursorPosition),
 						direction:
-							otherPosition < cursorPosition ? "forward" : otherPosition > cursorPosition ? "backward" : "none",
+							stalkPosition < cursorPosition ? "forward" : stalkPosition > cursorPosition ? "backward" : "none",
 					},
 				})
 			)
 		)
 	}
-	function arrowHomeEnd(formatter: Formatter, state: Readonly<State>, action: Action): Readonly<State> {
-		const result = State.copy(formatter.unformat(StateEditor.copy(state)))
-		let cursorPosition = result.selection.direction == "backward" ? result.selection.start : result.selection.end
-		let otherPosition = cursorPosition == result.selection.start ? result.selection.end : result.selection.start
-		cursorPosition =
-			action.key == "Home"
-				? 0
-				: action.key == "End"
-				? result.value.length
-				: result.selection.start == result.selection.end || action.shiftKey
-				? Math.min(Math.max(cursorPosition + (action.key == "ArrowLeft" ? -1 : 1), 0), result.value.length)
-				: action.key == "ArrowLeft"
-				? result.selection.start
-				: result.selection.end
-		otherPosition = action.shiftKey ? otherPosition : cursorPosition
-		result.selection.direction =
-			otherPosition < cursorPosition ? "forward" : otherPosition > cursorPosition ? "backward" : "none"
-		result.selection.start = Math.min(otherPosition, cursorPosition)
-		result.selection.end = Math.max(otherPosition, cursorPosition)
-		return result
-	}
-	function selectAll(formatter: Formatter, state: Readonly<State>): Readonly<State> {
-		console.log("selectAll")
-		const result = State.copy(formatter.unformat(StateEditor.copy(state)))
-		result.selection.start = 0
-		result.selection.end = result.value.length
-		result.selection.direction = "forward"
-		return result
-	}
-
-	function ctrlDeleteAction(formatter: Formatter, state: Readonly<State>, action: Action): Readonly<State> {
-		console.log("ctrlDeleteAction")
-		const cursorPosition = state.selection.direction == "backward" ? state.selection.start : state.selection.end
+	function ctrlBackspaceDelete(formatter: Formatter, state: Readonly<State>, action: Action): Readonly<State> {
+		const cursorPosition = Selection.getCursor(state.selection)
 		const adjecentIndex = getAdjecentWordBreakIndex(
 			state.value,
 			cursorPosition,
@@ -136,17 +95,34 @@ export namespace Action {
 		result.selection.end = result.selection.start
 		return result
 	}
-	function select(state: State, from: number, to: number): void {
-		console.log("select")
+	function arrowHomeEnd(state: State, action: Action) {
+		let cursorPosition = Selection.getCursor(state.selection)
+		let stalkPosition = Selection.getStalker(state.selection)
+		cursorPosition =
+			action.key == "Home"
+				? 0
+				: action.key == "End"
+				? state.value.length
+				: state.selection.start == state.selection.end || action.shiftKey
+				? Math.min(Math.max(cursorPosition + (action.key == "ArrowLeft" ? -1 : 1), 0), state.value.length)
+				: action.key == "ArrowLeft"
+				? state.selection.start
+				: state.selection.end
+		stalkPosition = action.shiftKey ? stalkPosition : cursorPosition
+		state.selection.direction =
+			stalkPosition < cursorPosition ? "forward" : stalkPosition > cursorPosition ? "backward" : "none"
+		state.selection.start = Math.min(stalkPosition, cursorPosition)
+		state.selection.end = Math.max(stalkPosition, cursorPosition)
+	}
+	function select(state: State, from: number, to: number, direction?: Direction): void {
 		state.selection.start = from
 		state.selection.end = to
+		direction && (state.selection.direction = direction)
 	}
 	function erase(state: State): void {
-		console.log("erase")
 		insert(state, "")
 	}
 	function insert(state: State, insertString: string): void {
-		console.log("insert inside", state, insertString)
 		state.value =
 			state.value.substring(0, state.selection.start) + insertString + state.value.substring(state.selection.end)
 		state.selection.start = state.selection.start + insertString.length
