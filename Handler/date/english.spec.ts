188d66606d4f173e73586df4c843913f6579ce6e
Qijie Yu <qijie@issuefab.com>
2021-12-02 16:48:14 +0100

    Clean up code and add some description in unit test.

--- Handler/date/english.spec.ts
+++ Handler/date/english.spec.ts
@@ -4,31 +4,31 @@ import { get } from "../index"
 
 describe("date english", () => {
 	const handler = get("date", "dd/MM/YYYY") as Formatter
-	it("date key event year last digit + auto dash", () => {
-		const result = Action.apply(handler, { value: "", selection: { start: 0, end: 0 } }, { key: "0" })
-		expect(result).toMatchObject({ value: "0", selection: { start: 1, end: 1 } })
+	it("first digit of day must be smaller than 4", () => {
+		const result = Action.apply(handler, { value: "", selection: { start: 0, end: 0 } }, { key: "3" })
+		expect(result).toMatchObject({ value: "3", selection: { start: 1, end: 1 } })
 	})
-	it("date key event year last digit + auto dash", () => {
+	it("second digit of day must be 0 or 1 if first digit is 3", () => {
 		const result = Action.apply(handler, { value: "3", selection: { start: 1, end: 1 } }, { key: "1" })
 		expect(result).toMatchObject({ value: "31", selection: { start: 2, end: 2 } })
 	})
-	it("date key event month trigger format", () => {
+	it("only splitter is allowed in third digit", () => {
 		const result = Action.apply(handler, { value: "31", selection: { start: 2, end: 2 } }, { key: "/" })
 		expect(result).toMatchObject({ value: "31/", selection: { start: 3, end: 3 } })
 	})
-	it("date key event month last digit trigger format", () => {
-		const result = Action.apply(handler, { value: "31/", selection: { start: 3, end: 3 } }, { key: "0" })
-		expect(result).toMatchObject({ value: "31/0", selection: { start: 4, end: 4 } })
+	it("first digit of month must be smaller than 2", () => {
+		const result = Action.apply(handler, { value: "31/", selection: { start: 3, end: 3 } }, { key: "1" })
+		expect(result).toMatchObject({ value: "31/1", selection: { start: 4, end: 4 } })
 	})
-	it("date key event month last digit trigger format", () => {
+	it("second digit of month must be smaller than 3", () => {
 		const result = Action.apply(handler, { value: "30/1", selection: { start: 4, end: 4 } }, { key: "2" })
 		expect(result).toMatchObject({ value: "30/12", selection: { start: 5, end: 5 } })
 	})
-	it("date key event 29 february (leap year) last digit trigger format", () => {
+	it("only splitter is allowed in sixth digit", () => {
 		const result = Action.apply(handler, { value: "31/10", selection: { start: 5, end: 5 } }, { key: "/" })
 		expect(result).toMatchObject({ value: "31/10/", selection: { start: 6, end: 6 } })
 	})
-	it("date key event 29 february last digit trigger format", () => {
+	it("only numbers are allowed in year", () => {
 		const result = Action.apply(handler, { value: "30/10/202", selection: { start: 9, end: 9 } }, { key: "1" })
 		expect(result).toMatchObject({ value: "30/10/2021", selection: { start: 10, end: 10 } })
 	})
