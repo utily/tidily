b06297d3764895b45e4eb3824e4e2a42bc91752a
Robin Nedergård <60611967+robinnedergard@users.noreply.github.com>
2023-10-17 16:32:14 +0200

    build: generate both es modules and commonjs (#69)
    * build: generate both es modules and commonjs
    
    * fix: fix test and remove some compile warnings

--- Handler/date/Base.ts
+++ Handler/date/Base.ts
@@ -17,17 +17,6 @@ export abstract class Base implements Converter<string>, Formatter {
 		return formatted.delete(this.separator)
 	}
 	abstract allowed(symbol: string, state: Readonly<State>): boolean
-	protected daysInMonth(value: string): number {
-		return (
-			32 -
-			Number.parseInt(
-				isoly.Date.next((this.fromString(value.padEnd(8, "1")) ?? "1970-01-01").substring(0, 8) + "28", 4).substring(
-					8,
-					10
-				)
-			)
-		)
-	}
 	protected validMonth(day: string, month: string, year?: string): boolean {
 		return (
 			new Date(Number.parseInt(year ?? "2004" /* must be leap year */), Number.parseInt(month), 0).getDate() >=
