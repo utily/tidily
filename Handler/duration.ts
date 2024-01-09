6ed0886c058e0d4bfe2bff807ee9bcf152a5f628
Bl3zzer <86045901+linusarnejohansson@users.noreply.github.com>
2024-01-09 13:41:54 +0100

    decimal entry of duration (#78)
    * decimal entry of duration
    
    * console.log removed
    
    * unit tests updated
    
    * regex expressions, improved matchning.
    
    * , support in fromString, limit minutes to 1 decimal
    
    * remove string check
    
    * formatting
    
    * formatting and comment removal
    
    * regex pattern

--- Handler/duration.ts
+++ Handler/duration.ts
@@ -6,41 +6,55 @@ import { StateEditor } from "../StateEditor"
 import { add } from "./base"
 
 class Handler implements Converter<{ hours: number; minutes: number } | undefined>, Formatter {
-	private pattern: RegExp
-	constructor(readonly suffix: string = "h") {
-		const pattern = this.suffix
-			.split("")
-			.map(symbol => symbol + "{0,1}")
-			.join("")
-		this.pattern = new RegExp(`^\\d*:{0,1}[0-5]{0,1}[0-9]{0,1}${pattern}$`)
-	}
+	// 									  normal time		  						 | 	decimal time
+	private pattern = /(^\d*:{0,1}[0-5]{0,1}[0-9]{0,1}$|^\d*[.,]{0,1}[0-9]{0,2}$)/
+	private decimal: boolean
 	toString(data: { hours: number; minutes: number } | undefined): string {
 		return `${data?.hours?.toString(10) ?? "0"}:${data?.minutes?.toString(10).padStart(2, "0") ?? "00"}`
 	}
 	fromString(value: string): { hours: number; minutes: number } | undefined {
-		const splitted = typeof value == "string" && value.split(":", 2).map(value => Number.parseInt(value))
-		return splitted
-			? {
-					hours: !splitted[0] || !Number.isFinite(splitted[0]) ? 0 : splitted[0],
-					minutes: !splitted[1] || !Number.isFinite(splitted[1]) ? 0 : splitted[1],
-			  }
-			: undefined
+		let result: undefined | { hours: number; minutes: number }
+		if (!this.decimal) {
+			const splitted = value.split(":", 2).map(value => Number.parseInt(value))
+			result = splitted
+				? {
+						hours: !splitted[0] || !Number.isFinite(splitted[0]) ? 0 : splitted[0],
+						minutes: !splitted[1] || !Number.isFinite(splitted[1]) ? 0 : splitted[1],
+				  }
+				: undefined
+		} else if (this.decimal) {
+			const splittedString = value.split(/[,.]/, 2)
+			if (splittedString)
+				splittedString[1] = splittedString[1].length == 1 ? splittedString[1] + "0" : splittedString[1]
+			const splitted = splittedString ? splittedString.map(value => Number.parseInt(value)) : undefined
+			result = splitted
+				? {
+						hours: !splitted[0] || !Number.isFinite(splitted[0]) ? 0 : splitted[0],
+						minutes:
+							!splitted[1] || !(0 <= splitted[1] && splitted[1] < 100)
+								? 0
+								: +(((splitted[1] / 100) * 60) % 1).toFixed(7),
+				  }
+				: undefined
+		}
+		return result
 	}
 	format(unformatted: StateEditor): Readonly<State> & Settings {
 		let result = unformatted
-		if (result.value.match(/^:/))
+		if (result.value.match(/^[.,:]/))
 			result = result.prepend("0")
-		if (result.value.length > 0)
-			result = result.suffix(this.suffix)
-		return { ...result, type: "tel", pattern: /^\d*:{0,1}[0-5]{0,1}[0-9]{0,1}(\sh{0,1}){0,1}$/ }
+		return { ...result, type: "tel", pattern: this.pattern }
 	}
 	unformat(formatted: StateEditor): Readonly<State> {
-		return formatted.delete(this.suffix)
+		if (formatted.value.includes(".") || formatted.value.includes(","))
+			this.decimal = true
+		else
+			this.decimal = false
+		return formatted
 	}
 	allowed(symbol: string, state: Readonly<State>): boolean {
-		const nextValue =
-			state.value.slice(0, state.selection.start) + symbol + state.value.slice(state.selection.end) + this.suffix
+		const nextValue = state.value.slice(0, state.selection.start) + symbol + state.value.slice(state.selection.end)
 		return !!nextValue.match(this.pattern)
 	}
 }
-add("duration", (argument: any[]) => new Handler(...argument))
+add("duration", () => new Handler())
