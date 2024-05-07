7b00b11f78cae1891e16a2f9bd67d637b8792f47
tovebr <70133569+tovebr@users.noreply.github.com>
2024-05-07 13:48:18 +0200

    Identifier bugfix: user cannot paste anything anymore (#90)
    * Identifier bugfix: user cannot paste anything anymore
    
    * lint

--- Handler/identifier.spec.ts
+++ Handler/identifier.spec.ts
@@ -6,19 +6,25 @@ import { get } from "./index"
 describe("identifier", () => {
 	const codeHandler = get("identifier-code") as Converter<"string" | unknown> & Formatter
 	it("sentence to code", () => {
-		const result = Action.apply(codeHandler, { value: "Lille John och -robin h00d", selection: { start: 0, end: 0 } })
-		expect(result).toMatchObject({ value: "lillejohnochrobinh00d", selection: { start: 0, end: 0 } })
+		const result = Action.apply(codeHandler, {
+			value: "/9 Lille John och -robin h00d",
+			selection: { start: 0, end: 0 },
+		})
+		expect(result).toMatchObject({ value: "9lillejohnochrobinh00d", selection: { start: 0, end: 0 } })
 		expect(codeHandler.toString(undefined)).toEqual("")
 		expect(codeHandler.fromString("")).toEqual(undefined)
 	})
 	const camelHandler = get("identifier-camel") as Formatter
 	it("sentence to camel", () => {
-		const result = Action.apply(camelHandler, { value: "Lille John och -robin h00d", selection: { start: 0, end: 0 } })
-		expect(result).toMatchObject({ value: "lilleJohnOchRobinH00d", selection: { start: 0, end: 0 } })
+		const result = Action.apply(camelHandler, {
+			value: "90Doe Lille// John och -robin h00d",
+			selection: { start: 0, end: 0 },
+		})
+		expect(result).toMatchObject({ value: "doeLilleJohnOchRobinH00d", selection: { start: 0, end: 0 } })
 	})
 	const pascalHandler = get("identifier-pascal") as Formatter
 	it("sentence to pascal", () => {
-		const result = Action.apply(pascalHandler, { value: "La Be Ro", selection: { start: 0, end: 0 } })
+		const result = Action.apply(pascalHandler, { value: "999 La Be Ro", selection: { start: 0, end: 0 } })
 		expect(result).toMatchObject({ value: "LaBeRo", selection: { start: 0, end: 0 } })
 	})
 	it("snake to pascal", () => {
@@ -29,6 +35,10 @@ describe("identifier", () => {
 	it("pascal to snake", () => {
 		const result = Action.apply(snakeHandler, { value: "FooBarBaz", selection: { start: 0, end: 0 } })
 		expect(result).toMatchObject({ value: "foo_bar_baz", selection: { start: 0, end: 0 } })
+	})
+	it("paste forbidden to snake", () => {
+		const result = Action.apply(snakeHandler, { value: "334 hej hej", selection: { start: 0, end: 0 } })
+		expect(result).toMatchObject({ value: "hej_hej", selection: { start: 0, end: 0 } })
 	})
 	it("attribute to snake", () => {
 		const result = Action.apply(snakeHandler, { value: "foo-bar-baz", selection: { start: 0, end: 0 } })
@@ -41,5 +51,12 @@ describe("identifier", () => {
 			selection: { start: 0, end: 0 },
 		})
 		expect(result).toMatchObject({ value: "lille-john-och-robin-h00d", selection: { start: 0, end: 0 } })
+	})
+	it("paste forbidden to attribute", () => {
+		const result = Action.apply(attributeHandler, {
+			value: "/90Doe _med Korv 8 /jeass",
+			selection: { start: 0, end: 0 },
+		})
+		expect(result).toMatchObject({ value: "doe-med-korv-8-jeass", selection: { start: 0, end: 0 } })
 	})
 })
