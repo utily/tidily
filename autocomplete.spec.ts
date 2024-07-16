879e107fdeb619f28b597e1dcf51659556fc2c98
Qijie Ye <qijie.ye@pax2pay.com>
2024-07-16 17:17:24 +0200

    rename

--- autocomplete.spec.ts
+++ autocomplete.spec.ts
@@ -3,6 +3,6 @@ import * as tidily from "./index"
 describe("Handler", () => {
 	const stateEditor = tidily.StateEditor.create()
 	const formatter = tidily.get("email")?.format(stateEditor)
-	const type = formatter?.type
+	const type = formatter?.autocomplete
 	it(`autocomplete type`, () => expect(type).toEqual("email"))
 })
