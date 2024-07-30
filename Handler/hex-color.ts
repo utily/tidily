dec289842873c9e8c66cf667b85c811850f3dec2
Tove Brandt <tove.brandt@pax2pay.com>
2024-07-30 15:06:10 +0200

    Hex color code working

--- Handler/hex-color.ts
+++ Handler/hex-color.ts
@@ -7,24 +7,28 @@ import { add } from "./base"
 
 class Handler implements Converter<string>, Formatter {
 	toString(value?: number | unknown): string {
-		return typeof value == "string" ? value : ""
+		return typeof value == "string" ? "#" + value : ""
 	}
 	fromString(value: string): string | undefined {
-		return value ? value : undefined
+		return value ? "#" + value : undefined
 	}
 	format(unformatted: StateEditor): Readonly<State> & Settings {
+		const result = unformatted.delete("#")
 		return {
-			...(unformatted.value ? unformatted.prepend("#") : unformatted),
+			...(result.value ? result.prepend("#") : result),
 			type: "text",
 			length: [4, 7],
-			pattern: /#[0-9a-fA-F]{3,6}/,
+			pattern: /^#([0-9a-f]{3}|[0-9a-f]{6})$/i,
 		}
 	}
 	unformat(formatted: StateEditor): Readonly<State> {
 		return formatted.delete("#")
 	}
 	allowed(symbol: string, state: Readonly<State>): boolean {
-		return (symbol >= "0" && symbol <= "9") || (symbol >= "a" && symbol <= "f") || (symbol >= "A" && symbol <= "F")
+		return (
+			((symbol >= "0" && symbol <= "9") || (symbol >= "a" && symbol <= "f") || (symbol >= "A" && symbol <= "F")) &&
+			state.value.length < 6
+		)
 	}
 }
 add("hex-color", () => new Handler())
