57e2c7cc70c4c802ed4e8ffbd56a89bad24a9729
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-10-17 09:10:08 +0200

    Add diners card number formatting (#100)

--- Handler/card-number.ts
+++ Handler/card-number.ts
@@ -16,7 +16,7 @@ class Handler implements Converter<string>, Formatter {
 	format(unformatted: StateEditor): Readonly<State> & Settings {
 		const issuer = getIssuer(unformatted.value)
 		const result = unformatted.map(
-			(symbol, index) => (index != 0 && index % 4 == 0 && index + 1 < issuer.length[0] ? " " : "") + symbol
+			(symbol, index) => (index + 1 < issuer.length[0] && issuer.spaceIndexes.includes(index) ? " " : "") + symbol
 		)
 		return {
 			...result,
@@ -41,8 +41,9 @@ add("card-number", () => new Handler())
 interface CardIssuer {
 	name: string
 	verification: RegExp
+	spaceIndexes: number[]
 	identification: RegExp
-	length: [number, number, number] // Max unformated, min formated, max formated
+	length: [number, number, number] // Max unformatted, min formatted, max formatted
 	icon: string
 }
 function getIssuer(value: string): CardIssuer & { name: string } {
@@ -57,6 +58,7 @@ function getIssuer(value: string): CardIssuer & { name: string } {
 const defaultIssuer: CardIssuer = {
 	name: "unknown",
 	verification: /^\d{19}$/,
+	spaceIndexes: [4, 8, 12],
 	identification: /^\d/,
 	length: [16, 16, 19],
 	icon: "generic",
@@ -70,18 +72,21 @@ const issuers: { [name: string]: Partial<CardIssuer> & { identification: RegExp 
 	},
 	dankort: {
 		verification: /^(5019)\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}$/,
+		spaceIndexes: [4, 8, 12],
 		identification: /^(5019)\d+/,
 		length: [16, 19, 19],
 		icon: "generic",
 	},
 	diners: {
-		verification: /^3(?:0[0-5]|[68][0-9])[0-9]\s[0-9]{4}\s[0-9]{4}\s[0-9]{2}$/,
+		verification: /^3(?:0[0-5]|[68][0-9])[0-9]\s[0-9]{6}\s[0-9]{4}$/,
 		identification: /^3(?:0[0-5]|[68][0-9])/,
-		length: [14, 17, 17],
+		spaceIndexes: [4, 10],
+		length: [14, 16, 16],
 		icon: "diners",
 	},
 	discover: {
 		verification: /^6(?:011|5[0-9]{2})\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}$/,
+		spaceIndexes: [4, 8, 12],
 		identification: /^6(?:011|5[0-9]{2})/,
 		length: [16, 19, 19],
 		icon: "generic",
@@ -90,11 +95,13 @@ const issuers: { [name: string]: Partial<CardIssuer> & { identification: RegExp 
 		verification:
 			/^((4026|4405|4508|4844|4913|4917)\s[0-9]{4}\s[0-9]{4}\s[0-9]{4})|((4175)\s(00)[0-9]{2}\s[0-9]{4}\s[0-9]{4})$/,
 		identification: /^(4026|417500|4405|4508|4844|4913|4917)/,
+		spaceIndexes: [4, 8, 12],
 		length: [16, 19, 19],
 		icon: "generic",
 	},
 	interpayment: {
 		verification: /^(636)[0-9]\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}$/,
+		spaceIndexes: [4, 8, 12],
 		identification: /^(636)/,
 		length: [16, 19, 19],
 		icon: "generic",
@@ -102,29 +109,34 @@ const issuers: { [name: string]: Partial<CardIssuer> & { identification: RegExp 
 	jcb: {
 		verification: /^((?:2131|1800)\s[0-9]{4}\s[0-9]{4}\s[0-9]{4})|(35[0-9]{2}\s[0-9]{4}\s[0-9]{4}\s[0-9]{4})$/,
 		identification: /^(?:2131|1800|35\d{3})/,
+		spaceIndexes: [4, 8, 12],
 		length: [16, 19, 19],
 		icon: "generic",
 	},
 	unionpay: {
 		verification: /^(62|88)[0-9]{2}\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}$/,
+		spaceIndexes: [4, 8, 12],
 		identification: /^(62|88)/,
 		length: [16, 19, 19],
 		icon: "generic",
 	},
 	maestro: {
 		verification: /^(5018|5020|5038|5612|5893|6304|6759|6761|6762|6763|0604|6390)\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}$/,
+		spaceIndexes: [4, 8, 12],
 		identification: /^(5018|5020|5038|5612|5893|6304|6759|6761|6762|6763|0604|6390)/,
 		length: [16, 19, 19],
 		icon: "maestro",
 	},
 	mastercard: {
 		verification: /^5[1-5][0-9]{2}\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}$/,
+		spaceIndexes: [4, 8, 12],
 		identification: /^5[1-5][0-9]/,
 		length: [16, 19, 19],
 		icon: "mastercard",
 	},
 	visa: {
 		verification: /^4[0-9]{3}\s[0-9]{4}\s[0-9]{4}\s[0-9](?:[0-9]{3})?$/,
+		spaceIndexes: [4, 8, 12],
 		identification: /^4[0-9]/,
 		length: [16, 16, 19],
 		icon: "visa",
