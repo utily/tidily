5cbb01af00b49fd4975037ec808c435ff5484ca6
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-03-12 15:47:34 +0100

    Limit integer to same number of digits as max

--- Handler/integer.ts
+++ Handler/integer.ts
@@ -49,7 +49,7 @@ class Handler implements Converter<number>, Formatter {
 		return formatted
 	}
 	allowed(symbol: string, state: Readonly<State>): boolean {
-		return symbol >= "0" && symbol <= "9"
+		return symbol >= "0" && symbol <= "9" && (this.max == undefined || this.max.toString().length > state.value.length)
 	}
 }
 add("integer", (argument?: any[]) => new Handler(argument?.[0] ?? {}))
